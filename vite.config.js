a80dd208fa5fe8b388aba115c069c695ad2e371b
YutakaNagai <ynagai103@gmail.com>
2023-09-01 23:42:51 +0900

    baseの変更

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from "vite";
+// Plugins
 import vue from "@vitejs/plugin-vue";
+
+// Utilities
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    base: "./",
 });
